bb84b038ce51b2cd8708bf4e55070a637e90c3d1
simonbassey <simon.dev.bassey@gmail.com>
2022-04-06 20:22:31 +0200

    Implemented tiny logic to create new accounts

--- app/src/main/java/com/zealmobile/netsharestudyapp/dao/UserAccountInterface.java
+++ app/src/main/java/com/zealmobile/netsharestudyapp/dao/UserAccountInterface.java
@@ -1,16 +1,23 @@
 package com.zealmobile.netsharestudyapp.dao;
 
+import com.zealmobile.netsharestudyapp.Models.CreateAccountResponse;
+import com.zealmobile.netsharestudyapp.Models.NewUserAccountModel;
 import com.zealmobile.netsharestudyapp.Models.UserAccountModel;
 import com.zealmobile.netsharestudyapp.RetrofitClient;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface UserAccountInterface {
     @GET("accounts")
     Call<List<UserAccountModel>> getUsers();
+
+    @POST("accounts/create")
+    Call<CreateAccountResponse> creaxteUserAccount(@Body NewUserAccountModel userAccount);
 
 
 }
