8ecdb78873dc1da9f10205ffb867f7ce0812b001
Confidence-bassey <confidencebass2@gmail.com>
2022-04-10 22:26:00 +0100

    Added a user dashboard

--- app/src/main/java/com/zealmobile/netsharestudyapp/User_Dashboard.java
+++ app/src/main/java/com/zealmobile/netsharestudyapp/User_Dashboard.java
@@ -2,9 +2,23 @@ package com.zealmobile.netsharestudyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class User_Dashboard extends AppCompatActivity {
+
+    public void showNavigationDrawer(View view){
+        ImageView myIview = (ImageView)findViewById(R.id.hamburgIcon);
+        myIview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_Dashboard.this, ActivitiesNavDrawer.class);
+                startActivity(intent);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
