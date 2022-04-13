66c47c112a44bcd24471b8ba8b565c8f5320e932
Confidence-bassey <confidencebass2@gmail.com>
2022-04-13 19:33:11 +0100

    Added a background color to activity_user_dashboard.xml file

--- app/src/main/java/com/zealmobile/netsharestudyapp/ActivitiesNavDrawer.java
+++ app/src/main/java/com/zealmobile/netsharestudyapp/ActivitiesNavDrawer.java
@@ -11,12 +11,7 @@ public class ActivitiesNavDrawer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities_nav_drawer);
-
-<<<<<<< HEAD
         Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-=======
-
->>>>>>> fa2c1863d059858f056db96fbb6c53cbec6d27c2
     }
 }
