25fdaa87d9359c3c350128f5a48f12065be14275
Confidence-bassey <confidencebass2@gmail.com>
2022-04-14 15:00:07 +0100

    Added a button to test logout functionality

--- app/src/main/java/com/zealmobile/netsharestudyapp/ActivitiesNavDrawer.java
+++ app/src/main/java/com/zealmobile/netsharestudyapp/ActivitiesNavDrawer.java
@@ -3,7 +3,10 @@ package com.zealmobile.netsharestudyapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivitiesNavDrawer extends AppCompatActivity {
 
@@ -13,5 +16,14 @@ public class ActivitiesNavDrawer extends AppCompatActivity {
         setContentView(R.layout.activity_activities_nav_drawer);
         Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button testbtn = (Button)findViewById(R.id.testing);
+        testbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivitiesNavDrawer.this, LogOut.class);
+                startActivity(intent);
+            }
+        });
     }
 }
