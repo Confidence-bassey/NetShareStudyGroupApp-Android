4e19453911b01058e2cdafc7046318f097c1612d
Confidence-bassey <confidencebass2@gmail.com>
2022-04-30 13:59:30 +0100

    Removed unused lines of code

--- app/src/main/java/com/zealmobile/netsharestudyapp/SignUp.java
+++ app/src/main/java/com/zealmobile/netsharestudyapp/SignUp.java
@@ -47,95 +47,73 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         _accountsAPI = RetrofitClient.getRetrofitInstance().create(UserAccountInterface.class);
-        
+
         txtFirstName = (EditText) findViewById(R.id.txt_signup_firstName);
         txtLastName = (EditText) findViewById(R.id.txt_signup_lastName);
         txtEmail = (EditText) findViewById(R.id.txt_signup_email);
         txtPhoneNumber = (EditText) findViewById(R.id.txt_signup_phone);
 
 
-        Button signUpButton = (Button)findViewById(R.id.btnSignUp);
+        Button signUpButton = (Button) findViewById(R.id.btnSignUp);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(TextUtils.isEmpty(txtFirstName.getText().toString())){
+                if (TextUtils.isEmpty(txtFirstName.getText().toString())) {
                     txtFirstName.setError("*required");
                     txtFirstName.requestFocus();
                     return;
                 }
-                if(TextUtils.isEmpty(txtLastName.getText().toString())){
+                if (TextUtils.isEmpty(txtLastName.getText().toString())) {
                     txtLastName.setError("*required");
                     txtLastName.requestFocus();
                     return;
                 }
-                if(TextUtils.isEmpty(txtEmail.getText().toString())){
+                if (TextUtils.isEmpty(txtEmail.getText().toString())) {
                     txtEmail.setError("*required");
                     txtEmail.requestFocus();
                     return;
                 }
-                if(TextUtils.isEmpty(txtPhoneNumber.getText().toString())){
+                if (TextUtils.isEmpty(txtPhoneNumber.getText().toString())) {
                     txtPhoneNumber.setError("*required");
                     txtPhoneNumber.requestFocus();
                     return;
                 }
-            }
-        });
-
-        Button btn = (Button)findViewById(R.id.btnSignUp);
-        Log.i("StudyGroup","Sign up button clicked");
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
                 handleCreateAccountButtonClick();
-                Log.i("StudyGroup","Sign up button called");
             }
         });
-
+        
     }
+        protected void handleCreateAccountButtonClick () {
+            NewUserAccountModel newUserAccount = new NewUserAccountModel();
+            newUserAccount.setFirstName(txtFirstName.getText().toString());
+            newUserAccount.setLastName(txtLastName.getText().toString());
+            newUserAccount.setEmail(txtEmail.getText().toString());
+            newUserAccount.setPhoneNumber(txtPhoneNumber.getText().toString());
 
- /*   @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.btnSignUp: {
-                handleCreateAccountButtonClick();
-                break;
-            }
-            default:
-                break;
+
+            Log.i("StudyGroup", String.format("sending user account payload: \n%s\n", new Gson().toJson(newUserAccount)));
+            //Todo: Here validate that every filled has been supplied a value
+            Call<CreateAccountResponse> createAccountApiCall = RetrofitClient.getUAccountInterface().creaxteUserAccount(newUserAccount);
+            createAccountApiCall.enqueue(
+                    new Callback<CreateAccountResponse>() {
+                        @Override
+                        public void onResponse(Call<CreateAccountResponse> call, Response<CreateAccountResponse> response) {
+                            Log.i("StudyGroup", String.format("create user account returned with response: \n%s", new Gson().toJson(response.body())));
+
+                            Toast.makeText(SignUp.this, "Successfully created account", LENGTH_LONG);
+
+                            Intent homeActivityIntent = new Intent(SignUp.this, AppHomeActivity.class);
+                            startActivity(homeActivityIntent);
+                        }
+
+                        @Override
+                        public void onFailure(Call<CreateAccountResponse> call, Throwable t) {
+                            Log.e("StudyGroup", String.format("Error occurred while creating new user account: %s", t.getMessage()), t);
+                            createAccountApiCall.cancel();
+                        }
+                    });
+
+
         }
-    }*/
 
-    protected void handleCreateAccountButtonClick() {
-        NewUserAccountModel newUserAccount = new NewUserAccountModel();
-                newUserAccount.setFirstName(txtFirstName.getText().toString());
-                newUserAccount.setLastName(txtLastName.getText().toString());
-                newUserAccount.setEmail(txtEmail.getText().toString());
-                newUserAccount.setPhoneNumber(txtPhoneNumber.getText().toString());
-
-
-        Log.i("StudyGroup", String.format("sending user account payload: \n%s\n", new Gson().toJson(newUserAccount)));
-        //Todo: Here validate that every filled has been supplied a value
-        Call<CreateAccountResponse> createAccountApiCall = RetrofitClient.getUAccountInterface().creaxteUserAccount(newUserAccount);
-        createAccountApiCall.enqueue(
-                new Callback<CreateAccountResponse>(){
-                    @Override
-                    public void onResponse(Call<CreateAccountResponse> call, Response<CreateAccountResponse> response) {
-                        Log.i("StudyGroup", String.format("create user account returned with response: \n%s", new Gson().toJson(response.body())));
-
-                        Toast.makeText(SignUp.this, "Successfully created account", LENGTH_LONG);
-
-                        Intent homeActivityIntent = new Intent(SignUp.this, AppHomeActivity.class);
-                        startActivity(homeActivityIntent);
-                    }
-
-                    @Override
-                    public void onFailure(Call<CreateAccountResponse> call, Throwable t) {
-                        Log.e("StudyGroup", String.format("Error occurred while creating new user account: %s", t.getMessage()), t);
-                        createAccountApiCall.cancel();
-                    }
-        });
-
-
-
-    }
 }
