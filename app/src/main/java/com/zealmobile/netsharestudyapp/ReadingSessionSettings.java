76da5ec5f625c46abf6efeac5a4d3e60e47481ad
Confidence-bassey <confidencebass2@gmail.com>
2022-05-05 23:57:42 +0100

    Created new fragments to implement reading session

---
+++ app/src/main/java/com/zealmobile/netsharestudyapp/ReadingSessionSettings.java
@@ -0,0 +1,36 @@
+package com.zealmobile.netsharestudyapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ReadingSessionSettings#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ReadingSessionSettings extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_reading_session_settings, container, false);
+        Button startSes = view.findViewById(R.id.startSession);
+        startSes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getChildFragmentManager();
+                SessionTitle readingSecTitle = new SessionTitle();
+                fm.beginTransaction().replace(R.id.readingSession, readingSecTitle).commit();
+            }
+        });
+        return view;
+    }
+}
